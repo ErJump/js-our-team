32d98ed3b480861149f0708b34f37b9620088248
ErJump <fedelijunior@gmail.com>
2022-06-28 13:53:35 +0200

    Bonus 1 Completed

--- js/myscript.js
+++ js/myscript.js
@@ -36,8 +36,11 @@ const container = document.getElementById('cardContainer');
 for (let i = 0; i < teamList.length; i++){
     const currentMember = teamList[i];
     const newDiv = document.createElement('div');
-    newDiv.classList.add('col-12');
-    newDiv.innerHTML = `${currentMember.name} ${currentMember.role} ${currentMember.img}`;
+    const newImg = document.createElement('img');
+    newImg.setAttribute('src', `img/${currentMember.img}`);
+    newDiv.classList.add('col-12', 'py-3');
+    newDiv.innerHTML = `${currentMember.name} ${currentMember.role}`;
     console.log(`${currentMember.name} ${currentMember.role} ${currentMember.img}`);
+    newDiv.append(newImg);
     container.append(newDiv);
 }
