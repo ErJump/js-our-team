fdf03d89e52deb7994929bb54297a566d8f51320
ErJump <fedelijunior@gmail.com>
2022-06-28 13:24:53 +0200

    Milestone I completed

---
+++ js/myscript.js
@@ -0,0 +1,37 @@
+let teamList = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Founder & CEO',
+        img : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        img : 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        img : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name : 'Scott Estrada',
+        role : 'Developer',
+        img : 'scott-estrada-developer.jpg',
+    },
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        img : 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+for (let i = 0; i < teamList.length; i++){
+    const currentMember = teamList[i];
+    console.log(`${currentMember.name} ${currentMember.role} ${currentMember.img}`);
+}
